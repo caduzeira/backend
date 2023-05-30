0e1a2f002a011334b56c1d1c9d30bf6d2102fe2c
caduzeira <carlosefaraujo333@gmail.com>
2023-05-29 21:02:04 -0300

    modulo12

---
+++ modulo-12-colecoes-part2/src/br/com/ceduardo/exercicios/NomesPorVirgula.java
@@ -0,0 +1,32 @@
+package br.com.ceduardo.exercicios;
+
+import java.util.*;
+
+public class NomesPorVirgula {
+    public static void main(String args[]){
+     Scanner s = new Scanner(System.in);
+
+     System.out.println("Digite nomes separados por virgula");
+     String entrada = s.next();
+
+     String[] lista = entrada.split(",");
+
+     List<String> listaNomes = new LinkedList<>();
+     for(String n : lista){
+         listaNomes.add(n);
+     }
+
+     System.out.println("****** Antes de ordenado ******");
+     for(String n:listaNomes){
+         System.out.println(n);
+     }
+     System.out.println("****** Depois de ordenado ******");
+     Collections.sort(listaNomes);
+     for(String n:listaNomes){
+         System.out.println(n);
+     }
+    }
+}
+
+System.out.println(separar[0]);
+            System.out.println(separar[1]);
