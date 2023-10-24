57ebf5d025b10dd08a13165c0d39087b52aaa492
Carlos Eduardo <carlosefaraujo333@gmail.com>
2023-10-24 19:18:14 -0300

    Comitando Projeto

---
+++ modulo-04-configurando-ide/src/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) throws Exception {
+        String argumentos = args[0];
+        if(argumentos.equals("DEV")){
+            System.out.println("DEV");
+        }
+    }
+}
